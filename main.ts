5161dcd7663db89494ef79b43770703fa49d186d
nano <learnwithmrnano@gmail.com>
2024-08-28 22:10:11 +0530

    commit. initial commit - working canvas on regex

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +16,59 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'lay-on-canvas',
+			name: 'Lay Matching Notes on Canvas',
+			callback: () => this.layOnCanvas(),
+		});
+
+	}
+
+	async layOnCanvas() {
+		const query = await this.getUserQuery();
+		const files = this.app.vault.getMarkdownFiles();
+		const matchingFiles = files.filter(file => this.matchesQuery(file, query));
+
+		if (matchingFiles.length === 0) {
+			new Notice('No matching notes found.');
+			return;
+		}
+		else {
+			new Notice(`${matchingFiles.length} notes found. E.g. ${matchingFiles[0].path}`);
+			console.log(matchingFiles);
+		}
+
+		await this.createCanvasWithNotes(matchingFiles);
+		new Notice(`${matchingFiles.length} notes added to the canvas.`);
+	}
+
+	async createCanvasWithNotes(files: TFile[]) {
+		const canvasFile = await this.app.vault.create('New Canvas.canvas', '{"nodes":[],"edges":[]}');
+		const leaf = await this.app.workspace.getLeaf(true);
+		await leaf.openFile(canvasFile);
+
+		if (leaf.view.getViewType() === 'canvas') {
+			console.log("Found canvas view");
+			const canvas = leaf.view as any; // Cast to any to bypass type checking
+			for (const file of files) {
+				canvas.canvas.createFileNode({ file, pos: { x: Math.random() * 1000, y: Math.random() * 1000 } });
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		} else {
+			new Notice('Failed to open canvas view');
+		}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+	async getUserQuery(): Promise<string> {
+		// Get user input for Regex or tag query
+		const query = await new Promise<string>(resolve => {
+			new PromptModal(this.app, 'Enter a Regex or tag to match notes:', resolve).open();
 		});
+		return query.trim();
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	matchesQuery(file: TFile, query: string): boolean {
+		const regex = new RegExp(query, 'i');
+		return regex.test(file.name);
 	}
 
 	onunload() {
@@ -91,44 +84,33 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class PromptModal extends Modal {
+
+	private title: string;
+	private onSubmit: (input: string) => void;
+
+	constructor(app: App, title: string, onSubmit: (input: string) => void) {
 		super(app);
+		this.title = title;
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.createEl('h1', { text: this.title });
+
+		const inputEl = contentEl.createEl('input', { type: 'text' });
+		inputEl.focus();
+
+		const submitBtn = contentEl.createEl('button', { text: 'Submit' });
+		submitBtn.onclick = () => {
+			this.onSubmit(inputEl.value);
+			this.close();
+		};
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
